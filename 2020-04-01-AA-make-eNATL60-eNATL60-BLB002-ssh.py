1453ecbecacfea02f065bbba21ab95f07c469a68
alberta <alberta@ige-template-domOK.lthe.local>
2020-05-04 09:43:40 +0200

    up

--- 2020-04-01-AA-make-eNATL60-eNATL60-BLB002-ssh.py
+++ 2020-04-01-AA-make-eNATL60-eNATL60-BLB002-ssh.py
@@ -1,37 +1,4 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-from dask_jobqueue import SLURMCluster 
-from dask.distributed import Client 
-  
-cluster = SLURMCluster(cores=28,name='make_profiles',walltime='00:30:00',job_extra=['--constraint=HSW24','--exclusive','--nodes=1'],memory='120GB',interface='ib0') 
-cluster.scale(280)
-cluster
-
-from dask.distributed import Client
-client = Client(cluster)
-client
-
-
-# In[2]:
-
-
-get_ipython().system('squeue -u albert7a')
-
-import time
-nb_workers = 0
-while True:
-    nb_workers = len(client.scheduler_info()["workers"])
-    if nb_workers >= 2:
-        break
-    time.sleep(1)
-print(nb_workers)
-
-
-# In[3]:
 
 
 import warnings
@@ -44,13 +11,10 @@ import os
 import time 
 import glob
 import numcodecs
-
+import datetime
 import zarr
 
-get_ipython().run_line_magic('matplotlib', 'inline')
 
-
-# In[4]:
 
 
 files=sorted(glob.glob('/store/CT1/hmg2840/lbrodeau/eNATL60/eNATL60-BLB002*-S/*/eNATL60*gridT-2D*nc'))
@@ -58,7 +22,6 @@ for file in files:
     print(file)
 
 
-# In[5]:
 
 
 # these are variables I just want to drop forever
@@ -74,13 +37,9 @@ open_kwargs = dict(drop_variables=(drop_vars + extra_coord_vars),
                    concat_dim="time_counter")  #, combine='nested')
 
 
-# In[6]:
-
 
 ds = xr.open_mfdataset(files, parallel=True, **open_kwargs)
 
-
-# In[7]:
 
 
 template = xr.open_dataset(files[0], decode_cf=False, decode_times=False)
@@ -94,30 +53,25 @@ ds
 del template
 
 
-# In[8]:
-
 
 ds
 
-
-# In[9]:
 
 
 compressor = numcodecs.Blosc(cname='snappy', clevel=6, shuffle=-1)
 encoding = {vname: {'compressor': compressor} for vname in ds.variables}
 
 
-# In[10]:
 
-
-outdir = '/store/albert7a/eNATL60/zarr/eNATL60-BLB002-SSH-1h'
+outdir = '/store/albert7a/eNATL60/zarr/eNATL60-BLB002-SSH-1h-new'
 ds = ds.chunk(chunks=dict(time_counter=240, y=240, x=480))
+
+print (str(datetime.datetime.now()))
 ds.to_zarr(outdir, encoding=encoding, mode="w")
+print (str(datetime.datetime.now()))
 
  
 
-
-# In[ ]:
 
 
 
